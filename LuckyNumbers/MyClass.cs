ba6bd27ca90516e031789171292c3d5794f98daa
itai bechor <itai.bechor@gmail.com>
2017-01-20 11:56:36 +0200

    Commit #2

--- LuckyNumbers/MyClass.cs
+++ LuckyNumbers/MyClass.cs
@@ -1,10 +1,39 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
 namespace LuckyNumbers
 {
-	public class MyClass
+	public class Lottery
 	{
-		public MyClass()
+		public IList<int> LuckyNumbers
+		{
+			get;
+			set;
+		}
+		public Lottery()
+		{
+			LuckyNumbers = new List<int>();
+		}
+		public void AddNumbers(int[] numbers)
+		{
+			LuckyNumbers.Add(numbers);
+
+		}
+		public void shuffleNumber()
 		{
 		}
+		public void NormalizeToLottoNumber()
+		{
+			foreach (var item in LuckyNumbers)
+
+			{
+				if (item > 37)
+				{
+
+				}
+			}
+		}
+
 	}
 }
